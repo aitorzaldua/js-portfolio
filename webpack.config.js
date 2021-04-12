9402c63c7bccbd1e1743265a361f5579a5bc65af
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-04-12 12:38:19 +0200

    april 12th

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require ('mini-css-extract-plugin');
+const CopyPlugin = require ('copy-webpack-plugin');
 
 module.exports = {
     entry:'./src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
+        assetModuleFilename: 'asset/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
@@ -26,6 +28,24 @@ module.exports = {
                 'css-loader',
                 'stylus-loader',
                 ]
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule: false,
+                    },
+                }
             }
 
         ]
@@ -37,6 +57,14 @@ module.exports = {
             filename: './index.html'
         }),
         new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
     ]
 }
 
